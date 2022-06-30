d2aee8aa29b079053d0298baf145dd74e9588bf6
Raiza gomes da silva <gomesraiza224@gmail.com>
2022-06-29 22:55:55 -0300

    corrigi conflito

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React, { Component } from "react";
 import axios from "axios";
-<<<<<<< HEAD
-import styled from "styled-components";
-import {Body, Header, MainHome} from "./components/styled"
-import PageCadastro from "./pages/pageCadastro/PageCadastro"
-import Carrinho from "./components/carrinho/Carrinho"
-
-=======
 import { Body, Header, Footer, Button } from "./components/styled";
 import Home from "./components/Home";
 import PageCadastro from "./pages/pageCadastro/PageCadastro";
 import PageCards from "./components/PageCards";
 import PageCarrinho from "./components/PageCarrinho";
->>>>>>> c5dec7b533df249c97db5850e191d9d67574df7e
 
 export default class App extends Component {
   state = {
@@ -40,23 +32,6 @@ export default class App extends Component {
       <Body>
         <Header>
           <h2>LabeNinjas</h2>
-<<<<<<< HEAD
-          <nav>
-            <button>Home</button>
-            <button>Carrinho</button>
-          </nav>
-        </Header>
-        <MainHome>
-          <h1>LabeNinjas</h1>
-          <h3>O talento certo no momento certo</h3>
-          <div>
-            <button>Quero ser um Ninja</button>
-            <button>Contratar um Ninja</button>
-          </div>
-        </MainHome>
-      
-      
-=======
           <div>
             <Button onClick={() => this.mudarTela("Home")}>Home</Button>
             <Button onClick={() => this.mudarTela("carinho")}>Carrinho</Button>
@@ -64,7 +39,6 @@ export default class App extends Component {
         </Header>
         {this.visualisarTela()}
         <Footer>Redes sociais</Footer>
->>>>>>> c5dec7b533df249c97db5850e191d9d67574df7e
       </Body>
     );
   }
