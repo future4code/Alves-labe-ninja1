6c2826bad81d5f39351e2c4b10b3adbeddd0f38b
Raiza gomes da silva <gomesraiza224@gmail.com>
2022-06-29 22:18:38 -0300

    carrinho

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
 import axios from "axios";
+<<<<<<< HEAD
 import styled from "styled-components";
 import {Body, Header, MainHome} from "./components/styled"
 import PageCadastro from "./pages/pageCadastro/PageCadastro"
 import Carrinho from "./components/carrinho/Carrinho"
 
+=======
+import { Body, Header, Footer, Button } from "./components/styled";
+import Home from "./components/Home";
+import PageCadastro from "./pages/pageCadastro/PageCadastro";
+import PageCards from "./components/PageCards";
+import PageCarrinho from "./components/PageCarrinho";
+>>>>>>> c5dec7b533df249c97db5850e191d9d67574df7e
 
 export default class App extends Component {
+  state = {
+    telaAtual: "Home",
+  };
+  mudarTela = (proximaTela) => {
+    this.setState({ telaAtual: proximaTela });
+  };
+
+  visualisarTela = () => {
+    switch (this.state.telaAtual) {
+      case "Home":
+        return <Home mudarTela={this.mudarTela} />;
+      case "cadastro":
+        return <PageCadastro mudarTela={this.mudarTela} />;
+      case "card":
+        return <PageCards mudarTela={this.mudarTela} />;
+      case "carinho":
+        return <PageCarrinho mudarTela={this.mudarTela} />;
+    }
+  };
+
   render() {
     return (
       <Body>
         <Header>
           <h2>LabeNinjas</h2>
+<<<<<<< HEAD
           <nav>
             <button>Home</button>
             <button>Carrinho</button>
@@ -27,6 +56,15 @@ export default class App extends Component {
         </MainHome>
       
       
+=======
+          <div>
+            <Button onClick={() => this.mudarTela("Home")}>Home</Button>
+            <Button onClick={() => this.mudarTela("carinho")}>Carrinho</Button>
+          </div>
+        </Header>
+        {this.visualisarTela()}
+        <Footer>Redes sociais</Footer>
+>>>>>>> c5dec7b533df249c97db5850e191d9d67574df7e
       </Body>
     );
   }
---
+++ src/pages/pageCadastro/PageCadastro.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 40vw;
+  margin: 10px;
+  justify-content: center;
+`;
+
+export default class PageCadastro extends React.Component {
+  render() {
+    return (
+      <Div>
+        <h1>Seja um ninja</h1>
+        <input placeholder="Titulo" />
+        <input placeholder="Descrição" />
+        <input placeholder="Preço" />
+        <select>
+          <option>Pix</option>
+          <option>Cartão de credito </option>
+          <option>Cartão de debito</option>
+          <option>Boleto</option>
+          <option>Paypal</option>
+        </select>
+        <input placeholder="Prazo do Serviço" type="date" value="" />
+        <button>Cadastrar Serviço</button>
+      </Div>
+    );
+  }
+}
